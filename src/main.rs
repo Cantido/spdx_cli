dff6a9b07251d39ef7781477ad11264c69680519
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-09-13 11:34:53 -0600

    add ls option

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,35 @@ struct License {
   is_deprecated_license_id: bool,
   name: String,
   license_id: String,
-  license_text: String,
+  license_text: Option<String>,
   is_osi_approved: bool,
   see_also: Vec<String>
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct LicenseList {
+  license_list_version: String,
+  licenses: Vec<License>
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   let args: Vec<String> = env::args().collect();
 
-  let license = &args[1];
+  if &args[1] == "ls" {
+    let licenses = reqwest::blocking::get("https://spdx.org/licenses/licenses.json")?
+         .json::<LicenseList>()?
+         .licenses;
 
-  let url = format!("https://spdx.org/licenses/{}.json", license);
+    for license in licenses.iter() {
+      println!("{} {}", license.license_id, license.name);
+    }
+  } else {
+    let url = format!("https://spdx.org/licenses/{}.json", &args[1]);
+    let license = reqwest::blocking::get(url)?
+        .json::<License>()?;
+    println!("{}", license.license_text.ok_or("No license text")?);
+  }
 
-  let license = reqwest::blocking::get(url)?
-      .json::<License>()?;
-
-  println!("{}", license.license_text);
   Ok(())
 }
